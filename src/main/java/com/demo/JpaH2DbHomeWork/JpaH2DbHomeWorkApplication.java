68c223a9318613795b98705da740d609fc6e0499
chuenningyeh <chuenningyeh@gmail.com>
2024-07-08 10:03:00 +0800

    init

---
+++ src/main/java/com/demo/JpaH2DbHomeWork/JpaH2DbHomeWorkApplication.java
@@ -0,0 +1,13 @@
+package com.demo.JpaH2DbHomeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaH2DbHomeWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaH2DbHomeWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/JpaH2DbHomeWork/JpaH2DbHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.JpaH2DbHomeWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaH2DbHomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
